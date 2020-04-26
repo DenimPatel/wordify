7c9903c0cac08a2aa9f27e1df3fb92a04cbbbb77
System Administrator <root@hanks-MacBook-Pro.local>
2020-04-26 01:30:31 -0400

    added pytest fixture for wordifier

--- test/test_wordifier.py
+++ test/test_wordifier.py
@@ -1,63 +1,65 @@
 """
 Test the wordifier methods
 """
+import pytest
 from algorithm.wordifier import Wordifier
+
+@pytest.fixture
+def ready_wordifier():
+    '''Returns a Wallet instance with a zero balance'''
+    wordifier = Wordifier()
+    wordifier.add_words_to_dictionary("data/google_10000_medium.txt")
+    return wordifier
 
 class TestWordifier:
     """
     Test wordifier methods
     """
     @staticmethod
-    def test_wordifier_words_to_numbers():
+    def test_wordifier_words_to_numbers(ready_wordifier):
         """
         Test method: words to numbers
         """
-        wordifier = Wordifier()
-        assert wordifier.words_to_number("HELLO") == "43556"
-        assert wordifier.words_to_number("hello") == "43556"
-        assert wordifier.words_to_number("painterhothot") == "7246837468468"
+        assert ready_wordifier.words_to_number("HELLO") == "43556"
+        assert ready_wordifier.words_to_number("hello") == "43556"
+        assert ready_wordifier.words_to_number("painterhothot") == "7246837468468"
 
     @staticmethod
-    def test_wordifier_words_to_numbers_2():
+    def test_wordifier_words_to_numbers_2(ready_wordifier):
         """
         Test method: words to numbers with numerals and special characters
         """
-        wordifier = Wordifier()
-        assert wordifier.words_to_number("803-308-hello") == "803-308-43556"
-        assert wordifier.words_to_number("803-308-HELLO") == "803-308-43556"
+        assert ready_wordifier.words_to_number("803-308-hello") == "803-308-43556"
+        assert ready_wordifier.words_to_number("803-308-HELLO") == "803-308-43556"
 
     @staticmethod
-    def test_wordifier_all_wordification3():
+    def test_wordifier_all_wordification3(ready_wordifier):
         """
         Test method: check atleast one response is there for known wodified query
         """
-        wordifier = Wordifier()
-        wordifier.add_words_to_dictionary("data/google_10000_medium.txt")
-        query = wordifier.words_to_number("18SOFTWARE")
-        response = wordifier.all_wordifications(query)
+        query = ready_wordifier.words_to_number("18SOFTWARE")
+        response = ready_wordifier.all_wordifications(query)
         assert len(response) >= 1
 
     @staticmethod
-    def test_wordifier_all_wordification():
+    def test_wordifier_all_wordification(ready_wordifier):
         """
         Test method: all wordification see known answer is present
         """
-        wordifier = Wordifier()
-        wordifier.add_words_to_dictionary("data/google_10000_medium.txt")
-        query = wordifier.words_to_number("18PAINTPAINT")
-        response = wordifier.all_wordifications(query)
+        ready_wordifier.add_words_to_dictionary("data/google_10000_medium.txt")
+        query = ready_wordifier.words_to_number("18PAINTPAINT")
+        response = ready_wordifier.all_wordifications(query)
         assert len(response) >= 1
         assert "18PAINTPAINT" in response
 
     @staticmethod
-    def test_wordifier_all_wordification2():
+    def test_wordifier_all_wordification2(ready_wordifier):
         """
         Test method: all wordification see all answer has correct phone number
         """
-        wordifier = Wordifier()
-        wordifier.add_words_to_dictionary("data/google_10000_medium.txt")
-        query = wordifier.words_to_number("18PAINTSAINT")
-        response = wordifier.all_wordifications(query)
+        ready_wordifier.add_words_to_dictionary("data/google_10000_medium.txt")
+        query = ready_wordifier.words_to_number("18PAINTSAINT")
+        response = ready_wordifier.all_wordifications(query)
         assert len(response) >= 1
         for each in response:
-            assert wordifier.words_to_number(each) == query
+            assert ready_wordifier.words_to_number(each) == query
